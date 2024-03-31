490d9845edf1a8e7aceb4e0819bf792aa1625a47
Daustinn <daustinn.dev@gmail.com>
2024-03-31 00:02:35 -0500

    Update metadata in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,10 +10,38 @@ import {
 } from 'ui/components'
 
 export const metadata: Metadata = {
+  title: 'David Bendezú (Daustinn) - Front-End Developer and UI Designer',
+  description:
+    'Front-End Developer and UI Designer based in Lima, Perú with a passion for web development and open source software.',
+  keywords: [
+    'David Bendezú',
+    'Daustinn',
+    'Front-End',
+    'Developer',
+    'UI Designer',
+    'Web Developer',
+    'Open Source',
+    'React',
+    'Next.js',
+    'Node.js',
+    'JavaScript',
+    'TypeScript',
+    'CSS',
+    'HTML',
+    'Web Development',
+    'Frontend Development',
+    'Backend Development',
+    'Fullstack Development',
+    'Lima',
+    'Perú'
+  ],
+  abstract:
+    'Front-End Developer and UI Designer based in Lima, Perú with a passion for web development and open source software.',
   openGraph: {
     type: 'website',
     title: 'Daustinn',
-    description: 'Personal website of Dustin Heisey.',
+    description:
+      'More than 3 years creating beautiful and minimalist websites and applications with the latest technologies.',
     images: {
       hash: 'og-image-home',
       host: 'https://daustinn.com',
