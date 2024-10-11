8baf586e93502d2da1cd8600ddd193197c47e23c
Daustinn <daustinn.music@gmail.com>
2024-10-10 23:00:16 -0500

    refactor: Remove unused Portal component and related files

--- ui/lab/portal/index.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import * as PortalPrimitive from '@radix-ui/react-portal'
-import React from 'react'
-
-export const Portal = ({ children }: { children: React.ReactNode }) => {
-  return <PortalPrimitive.Root asChild>{children}</PortalPrimitive.Root>
-}
