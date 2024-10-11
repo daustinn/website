346cf32b9f0476c26610c169923de45aaaad647c
Daustinn <daustinn.music@gmail.com>
2024-10-10 23:00:56 -0500

    refactor: Remove unused UI components and related files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {}
 
-export default nextConfig;
+import createNextIntlPlugin from 'next-intl/plugin'
+
+const withNextIntl = createNextIntlPlugin()
+
+export default withNextIntl(nextConfig)
