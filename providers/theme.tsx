3697bb99ced1ceb2bf84f39170df9b2358819526
Daustinn <daustinn.music@gmail.com>
2024-10-10 22:58:53 -0500

    refactor: Update ThemeProvider to include default theme and disable transition on change

--- providers/theme.tsx
+++ providers/theme.tsx
@@ -5,5 +5,14 @@ import { ThemeProvider as NextThemeProvider } from 'next-themes'
 import type { ThemeProviderProps } from 'next-themes/dist/types'
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemeProvider {...props}>{children}</NextThemeProvider>
+  return (
+    <NextThemeProvider
+      defaultTheme={'system'}
+      disableTransitionOnChange={true}
+      attribute="class"
+      {...props}
+    >
+      {children}
+    </NextThemeProvider>
+  )
 }
