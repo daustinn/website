cb03072581d7cc0866984142b6090f9d2d0fa713
Daustinn <daustinn.music@gmail.com>
2025-03-23 23:49:46 -0500

    Refactor: Simplify Header component by removing unused hooks and optimizing structure

--- app/(childs)/header.tsx
+++ app/(childs)/header.tsx
@@ -4,14 +4,7 @@ import ThemeToggle from '~app/theme-toggle'
 import LanguageToggle from '~app/language-toggle'
 import { usePathname, useRouter } from 'next/navigation'
 import { ArrowBack } from '~ui/icons'
-import {
-  DATA_HEADER_KEY,
-  DATA_LANGUAGE_KEY,
-  DATA_THEME_KEY,
-  DICTIONARIES
-} from '~const'
-import { useDocumentAtribute } from '~hooks/use-document-atribute'
-import { cn } from '~utils'
+import { DICTIONARIES } from '~const'
 import React from 'react'
 
 export default function Header() {
@@ -21,38 +14,20 @@ export default function Header() {
   const siteName =
     DICTIONARIES[firstPath as keyof typeof DICTIONARIES] || firstPath
 
-  const dataValue = useDocumentAtribute(
-    DATA_HEADER_KEY,
-    'dark:text-white text-black'
-  )
-
-  const themeDataValue = useDocumentAtribute(DATA_THEME_KEY, 'visible')
-  const languageDataValue = useDocumentAtribute(DATA_LANGUAGE_KEY, 'visible')
-
-  const windowHistory = React.useMemo(() => window?.history?.length, [])
   return (
-    <div
-      className={cn(
-        'p-1.5 pointer-events-none px-2 md:fixed relative z-10 top-0 flex inset-x-0',
-        dataValue
-      )}
-    >
+    <div className="p-1.5 pointer-events-none px-2 md:fixed relative z-10 top-0 flex inset-x-0 dark:text-white text-black">
       <div className="grow flex items-center">
-        {windowHistory > 1 && (
-          <button
-            onClick={() => router.back()}
-            className="flex items-center font-semibold text-sm tracking-tight pointer-events-auto gap-2"
-          >
-            <div className="bg-stone-500/30 rounded-full p-2">
-              <ArrowBack size={18} />
-            </div>
-            {siteName}
-          </button>
-        )}
+        <button
+          onClick={() => router.back()}
+          className="flex px-2 items-center font-semibold text-sm tracking-tight pointer-events-auto gap-2"
+        >
+          <ArrowBack size={18} />
+          {siteName}
+        </button>
       </div>
       <div className="flex pointer-events-auto gap-3">
-        {themeDataValue !== 'hidden' && <ThemeToggle />}
-        {languageDataValue !== 'hidden' && <LanguageToggle />}
+        <ThemeToggle />
+        <LanguageToggle />
       </div>
     </div>
   )
